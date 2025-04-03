a9f0421cb83e789416c8883d7a3263b612cf243a
ChandruJ <2k21it08@kiot.ac.in>
2025-04-03 22:20:42 +0530

    commit

---
+++ src/test/java/com/definitions/Hooks.java
@@ -0,0 +1,20 @@
+package com.definitions;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import io.cucumber.java.Scenario;
+import com.utilities.HelperClass;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+	@Before
+	public static void setUp() {
+		HelperClass.setUpDriver();
+	}
+	@After
+	public static void teardown(Scenario scenario) {
+		HelperClass.teardown();
+	}
+}
---
+++ src/test/java/com/pages/HomePageLocaters.java
@@ -0,0 +1,9 @@
+package com.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePageLocaters {
+	@FindBy(xpath="//h6[text()='Dashboard']")
+	public WebElement dashboard;
+}
